f9625df08ceb0b35eb8c53df8ad117adb47f9c39
sdjdd <i@sdjdd.com>
2020-06-04 16:59:34 +0800

    dev: empty app info

--- env.ts
+++ env.ts
@@ -1,6 +1,6 @@
-const myAppId = 'oY2aqSxhKvtL2URCcKNehatA-gzGzoHsz';
-const myAppKey = 'yr6xMoCYahu75yy1uRug7Vmv';
-const myServerURL = 'https://oy2aqsxh.lc-cn-n1-shared.com';
+const myAppId = '';
+const myAppKey = '';
+const myServerURL = '';
 
 export const appId = process.env.LC_APP_ID || myAppId;
 export const appKey = process.env.LC_APP_KEY || myAppKey;
