caa4a2c8d8bc4784ae5e440794f505439e3db5ab
sdjdd <i@sdjdd.com>
2020-06-04 11:58:16 +0800

    dev: add env.ts

---
+++ env.ts
@@ -0,0 +1,5 @@
+export const appId = process.env.LC_APP_ID || '';
+export const appKey = process.env.LC_APP_KEY || '';
+export const serverURL = process.env.LC_SERVER_URL || '';
+
+export default { appId, appKey, serverURL };
