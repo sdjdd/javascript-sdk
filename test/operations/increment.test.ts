ec7ebf9f652eb2017b0d249b64b79d7a004aee2e
sdjdd <i@sdjdd.com>
2020-06-11 17:20:22 +0800

    dev: add coverage to ci

--- test/operations/increment.test.ts
+++ test/operations/increment.test.ts
@@ -1,1 +1,14 @@
 import 'should';
+
+import { IncrementOperation, NAME_INCREMENT } from '../../src/core/operations';
+
+describe('IncrementOperation', function () {
+  describe('#constructor', function () {
+    it('should set correct name and amount', function () {
+      const amount = 12345;
+      const op = new IncrementOperation(amount);
+      op.name.should.eql(NAME_INCREMENT);
+      op.extra.amount.should.eql(amount);
+    });
+  });
+});
